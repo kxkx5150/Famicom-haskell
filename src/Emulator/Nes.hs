af6990c14b9523e624286d80b1b0e786fda2e62a
Kunihiro Ando <senna5150ando@gmail.com>
2022-04-21 19:00:29 +0900

    commit

--- src/Emulator/Nes.hs
+++ src/Emulator/Nes.hs
@@ -42,8 +42,35 @@ import           Prelude                 hiding ( read
                                                 , replicate
                                                 )
 
+
 newtype Emulator a = Emulator { unNes :: ReaderT Nes IO a }
   deriving (Monad, Applicative, Functor, MonadIO, MonadReader Nes)
+
+data Sprite = Sprite {
+  sIndex         :: Int,
+  sCoords        :: Coords,
+  sTileIndexByte :: Word8,
+  sAttributeByte :: Word8,
+  sPattern       :: Word32,
+  sPriority      :: Word8
+} deriving (Show, Eq)
+
+type Coords = (Int, Int)
+
+type Color = (Word8, Word8, Word8)
+
+data IncrementMode = Horizontal | Vertical
+
+data SpriteSize = Normal | Double
+
+data ColorMode = Color | Grayscale
+
+data Visibility = Hidden | Shown
+
+data Interrupt
+  = IRQ
+  | NMI
+  deriving (Eq, Show)
 
 data Nes = Nes
   { cpu    :: CPU
@@ -62,10 +89,79 @@ data CPU = CPU
   , ram       :: VUM.IOVector Word8
   }
 
+data PPU = PPU {
+  -- Misc
+  ppuCycles             :: IORef Int,
+  scanline              :: IORef Int,
+  frameCount            :: IORef Int,
+  writeToggle           :: IORef Bool,
+  ppuRegister           :: IORef Word8,
+  oddFrame              :: IORef Bool,
+  -- Data
+  oamData               :: VUM.IOVector Word8,
+  nameTableData         :: VUM.IOVector Word8,
+  paletteData           :: VUM.IOVector Word8,
+  screen                :: VUM.IOVector Word8,
+  -- Addresses
+  currentVramAddress    :: IORef Word16,
+  tempVramAddress       :: IORef Word16,
+  oamAddress            :: IORef Word8,
+  -- NMI
+  nmiOutput             :: IORef Bool,
+  nmiOccurred           :: IORef Bool,
+  nmiDelay              :: IORef Word8,
+  nmiPrevious           :: IORef Bool,
+  -- Control register bits
+  nameTable             :: IORef Word16,
+  incrementMode         :: IORef IncrementMode,
+  spriteTable           :: IORef Word16,
+  bgTable               :: IORef Word16,
+  spriteSize            :: IORef SpriteSize,
+  -- Mask register bits
+  colorMode             :: IORef ColorMode,
+  leftBgVisibility      :: IORef Visibility,
+  leftSpritesVisibility :: IORef Visibility,
+  bgVisibility          :: IORef Bool,
+  spriteVisibility      :: IORef Bool,
+  intensifyReds         :: IORef Bool,
+  intensifyGreens       :: IORef Bool,
+  intensifyBlues        :: IORef Bool,
+  -- Status register bits
+  spriteOverflow        :: IORef Bool,
+  spriteZeroHit         :: IORef Bool,
+  -- verticalBlank         :: IORef Bool,
+  -- Scroll register
+  fineX                 :: IORef Word8,
+  -- Data register
+  dataV                 :: IORef Word8,
+  -- Temp vars
+  nameTableByte         :: IORef Word8,
+  attrTableByte         :: IORef Word8,
+  loTileByte            :: IORef Word8,
+  hiTileByte            :: IORef Word8,
+  tileData              :: IORef Word64,
+  sprites               :: IORef (V.Vector Sprite)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 initNes :: BS.ByteString -> Emulator a -> IO a
 initNes bs (Emulator reader) = do
   cart <- Cartridge.parse bs
   nes  <- newNes cart
+  ppu <- newPPU
   runReaderT reader nes
 
 newNes :: Cartridge -> IO Nes
@@ -86,6 +182,101 @@ newCPU = do
   ram    <- VUM.replicate 65536 0x0
   pure $ CPU pc sp a x y p cycles ram
 
+newPPU :: IO PPU
+newPPU = do
+  -- Misc
+  cycles <- newIORef 0
+  scanline <- newIORef 0
+  frameCount <- newIORef 0
+  writeToggle <- newIORef False
+  ppuRegister <- newIORef 0x0
+  oddFrame <- newIORef False
+  -- Data
+  oamData <- VUM.replicate 0x100 0x0
+  nameTableData <- VUM.replicate 0x800 0x0
+  paletteData <- VUM.replicate 0x20 0x0
+  screen <- VUM.replicate (256 * 240 * 3) 255
+  -- Addresses
+  currentVramAddress <- newIORef 0x0
+  tempVramAddress <- newIORef 0x0
+  oamAddress <- newIORef 0x0
+  -- NMI
+  nmiOutput <- newIORef False
+  nmiOccurred <- newIORef False
+  nmiDelay <- newIORef 0
+  nmiPrevious <- newIORef False
+  -- Control register
+  nameTable <- newIORef 0x2000
+  incrementMode <- newIORef Horizontal
+  spriteTable <- newIORef 0x0000
+  bgTable <- newIORef 0x0000
+  spriteSize <- newIORef Normal
+  -- Mask register
+  colorMode <- newIORef Color
+  leftBgVis <- newIORef Hidden
+  leftSpritesVis <- newIORef Hidden
+  bgVis <- newIORef False
+  spriteVis <- newIORef False
+  intensifyReds <- newIORef False
+  intensifyGreens <- newIORef False
+  intensifyBlues <- newIORef False
+  -- Status register
+  spriteOverflow <- newIORef False
+  spriteZeroHit <- newIORef False
+  -- Scroll register
+  fineX <- newIORef 0x0
+  -- Data register
+  dataV <- newIORef 0x0
+  -- Temp vars
+  nameTableByte <- newIORef 0x0
+  attrTableByte <- newIORef 0x0
+  loTileByte <- newIORef 0x0
+  hiTileByte <- newIORef 0x0
+  tileData <- newIORef 0x0
+  sprites <- newIORef V.empty
+
+  pure $ PPU
+    -- Misc
+    cycles scanline frameCount writeToggle ppuRegister oddFrame
+    -- Data
+    oamData nameTableData paletteData screen
+    -- Addresses
+    currentVramAddress tempVramAddress oamAddress
+    -- NMI
+    nmiOutput nmiOccurred nmiDelay nmiPrevious
+    -- Control register
+    nameTable incrementMode spriteTable bgTable spriteSize
+    -- Mask register
+    colorMode leftBgVis leftSpritesVis bgVis spriteVis
+    intensifyReds intensifyGreens intensifyBlues
+    -- Status register
+    spriteOverflow spriteZeroHit
+    -- Scroll register
+    fineX
+    -- Data register
+    dataV
+    -- Temp vars
+    nameTableByte attrTableByte loTileByte hiTileByte tileData sprites
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 with :: (Nes -> b) -> (b -> IO a) -> Emulator a
 with field f = do
   nes <- ask
@@ -99,3 +290,6 @@ storeReg field v = with (field . cpu) (`modifyIORef'` const v)
 
 modReg :: (CPU -> IORef b) -> (b -> b) -> Emulator ()
 modReg field v = with (field . cpu) (`modifyIORef'` v)
+
+
+
